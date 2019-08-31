274485e2dc441b826cffc41cdef5d6d33bdd134d
Liu Ze <2411509324@qq.com>
2019-08-31 22:17:43 +0800

    add router

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+const bodyParser = require('body-parser');
+
 const path = require('path');
 
 const port = process.env.PORT || 5200;
 
 const hostName = '192.168.2.129';
 
-const { mongoUrl, options } = require('./config/keys');
+const {
+    mongoUrl,
+    options
+} = require('./config/keys');
 
 const users = require('./routes/api/users');
 
@@ -20,13 +25,18 @@ app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.use('/api/users', users);
 
 app.listen(port, hostName, (err) => {
     if (err) {
         console.error(err);
-    }
-    else {
+    } else {
         mongoose.connect(mongoUrl, options)
             .then(() => console.log(`数据库连接成功`))
             .catch(err => console.error(err));
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test',(req, res,next) => {
-    res.json({mas:'login works'});
+const User = require('../../models/User');
+
+router.get('/test', (req, res, next) => {
+    res.json({
+        mas: 'login works'
+    });
+});
+
+router.post('/register', (req, res, next) => {
+    User.findOne({
+            email: req.body.emai
+        })
+        .then((user) => {
+            if (user) {
+                return res.status(400).json({
+                    message: '邮箱已被注册',
+                    code: 0
+                })
+            } else {
+                const newUser = new User({
+                    name: req.body.uaerName,
+                    email: req.body.email,
+                    avatr,
+                    password: req.body.password
+                })
+            }
+        })
 });
 
 module.exports = router;
